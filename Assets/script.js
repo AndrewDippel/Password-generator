dc160c804ff48f035f0503c1035f10e5e1b31cf4
Andrew Dippel <a.t.dippel@gmail.com>
2022-08-14 13:24:17 +1000

    added arrays, prompt and if statement for empty or invalid input

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var characters = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":","\"","\\"]
+
+function generatePassword() {
+  var passwordLength = window.prompt("Enter password length");
+  console.log(passwordLength);
+
+  if (passwordLength <8 || passwordLength >128 || !passwordLength ){
+    alert ("please enter number between 8 and 128");
+  }
+  return;
+}
 
 // Write password to the #password input
 function writePassword() {
