cec8b037ddc512be1ba94108b99c27411a8b5c01
Andrew Dippel <a.t.dippel@gmail.com>
2022-08-15 18:13:46 +1000

    added generated password to display box and added some comments

--- Assets/script.js
+++ Assets/script.js
@@ -5,26 +5,27 @@ var uppercase = []
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var symbols = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":","\"","\\"]
 
+//for this function it is timsing the decimal we generate by the max number of the array and then rounding down so we always stay within the length of the array
 function randomIndex(max) {
      var rand = Math.random()
      return Math.floor(rand*max)
 }
-
 function randomItem(list) {
-  //var randomI = 
   return list[randomIndex(list.length)]
 }
 
+//this will be the window promt to enter the length of your password
 function generatePassword() {
   var passwordLength = window.prompt("Enter password length");
-  console.log(passwordLength);
-
   var passwordNum = parseInt(passwordLength)
 
+  //this is promting an alert if the password isn't within the specified range or is left empty
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength  > 128 || !passwordLength ){
     window.alert("please enter number between 8 and 128");
     return;
   }
+
+  //these variables are the pop up windows to select what characters are needed for your password
   var adduppercase = window.confirm("would you like to add uppercase letters?")
   var addlowercase = window.confirm("would you like to add lowercase letters?")
   var addnumbers = window.confirm("would you like to add numbers?")
@@ -32,6 +33,7 @@ function generatePassword() {
 
   var options = []
 
+  //this is pushing the spread of each array chosen into a seperate variable creating a new array
   for (var i = 0; i < lowercase.length; i++) {
     uppercase.push(lowercase[i].toUpperCase())
   }
@@ -42,18 +44,22 @@ function generatePassword() {
     options.push(...lowercase)
   }
   if (addnumbers) {
-    options.push(numbers)
+    options.push(...numbers)
   }
   if (addsymbols) {
-    options.push(symbols)
+    options.push(...symbols)
   }
 
   var generatedPassword = ""
 
+  //here we are combining the random number generater with the options array to pull the random charactors for the password and make it a string
   for (var i = 0; i < passwordLength; i++) {
     var randomChar = randomItem(options)
-    console.log(randomChar)
+    generatedPassword += randomChar
   }
+
+  //this will display the generated password string in the text box 
+  return generatedPassword
 }
 
 // Write password to the #password input
